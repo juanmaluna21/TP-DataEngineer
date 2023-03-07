c1c89afd7dd6e58d84d21d0ab16d43e30d18f86c
Juan Luna <juanluna@Juans-MacBook-Air.local>
2023-03-07 02:06:15 -0300

    check

--- main.py
+++ main.py
@@ -1,28 +1,62 @@
-from fastapi import FastAPI
 import pandas as pd
+import transformation as tr
+import json
+from fastapi import FastAPI
 
 #Titulo y descripción de la API
-app = FastAPI(title= 'Base de datos de Peliculas',
-              description= 'A través de esta API se puede encontrar las peliculas/series de las plataformas: Amazon, Disney Plus, Hulu y Netflix',
+app = FastAPI(title= 'Movies and Series database',
+              description= 'Through this API you can find the movies/series of the platforms: Amazon, Disney Plus, Hulu and Netflix',
               )
 
-df= pd.read_csv(r'df_final.to_csv')
-
+# welcome
 @app.get('/')
-async def index ():
-    return {"Bienvenido!!!"}
+async def index():
+    return {"Welcome!!!"}
 
-@app.get('about')
+@app.get('/about')
 async def about():
     return 'Proyecto Data Engineer para SoyHenry'
 
-@app.get('cantidad_keyword')
-async def cantidad_keyword (data_frame: str, keyword: str):
+# files load
+platforms = {'amazon': tr.df_amazon, 'disney':tr.df_disney, 'hulu': tr.df_hulu, 'netflix':tr.df_netflix}
+
+# general file load
+@app.on_event('startup')
+async def startup():
+    global df
+    df= pd.read_csv(r'df_final.csv')
+    
+
+#QUERY
+
+# 1) Number of times a keyword appears in the title of movies/series, by platform
+@app.get('/keyword_quantity')
+async def keyword_quantity (data_frame:str, keyword:str):
     contador = 0
     lista = []
-    for palabra in data_frame.title:
+    for palabra in platforms[data_frame].title:
         palabra = palabra.split()
         for elemento in palabra:
             lista.append(elemento)
     contador = lista.count(keyword)
     return contador
+
+# 2) Number of films by platform with a score greater than XX in a given year
+@app.get('/film_by_score')
+async def film_by_score(data_frame:str, anio:int, puntaje:int):
+    return tr.film_by_score(platforms[data_frame], anio, puntaje)
+
+# 3) Second highest scoring film for a given platform, based on alphabetical order of titles
+@app.get('/second_max')
+def second_max(data_frame:str):
+    return json.loads(tr.second_max(platforms[data_frame]))
+
+# 4) Film that lasted the longest according to year, platform and type of duration
+@app.get('/max_duration')
+def max_duration(data_frame:str, tipo:str):
+    return json.loads(tr.max_duration(platforms[data_frame], tipo))
+
+# 5) Number of series and movies by score
+@app.get('/movies_series_byscore')
+def movies_series_byscore (data_frame):
+    return json.loads(tr.movies_series_byscore(platforms[data_frame]))
---
+++ transformation.py
@@ -0,0 +1,57 @@
+import pandas as pd
+import json as json
+
+def transformation (df,letras):
+    df.rename(columns={'show_id':'id'},inplace= True) # renombro la columna "show_id" a "id"
+    df['id'] = [letras + elemento for elemento in df['id']] # agrego la letra correspodiente
+    df.set_index('id',inplace=True) # asigno a la columna "id" como indice
+    df['rating'].fillna('G',inplace=True) # relleno los valores NaN de la columna "rating" con "G"
+    df['date_added']= pd.to_datetime(df['date_added']) #cambio el formato de las fechas
+    string_columns = [column for column in df.columns if df[column].dtype == 'object'] #elijo cual de las columnas es de tipo 'object'
+    df[string_columns] = df.loc[:,string_columns].apply(lambda x: x.str.lower()) # transformo en minusculas a las columnas de tipo object
+    df['duration'].fillna ('NaN NaN', inplace=True) #reemplazo el los valores faltantes de la columna duration por "NaN NaN" con el fin de poder dividirla depues
+    duracion = df['duration'].str.split() #separo la columna "duration" en 2
+    duration_int = [i[0] for i in duracion] # asigno los primeros valores a "duration_int"
+    duration_type= [i[1] for i in duracion] # asigno los primeros valores a "duration_type"
+    df.insert(9,'duration_int',duration_int) # inserto la columna "duration_int"
+    df.insert(10,'duration_type',duration_type) # inserto la columna "duration_type"
+    df.drop(columns= 'duration', inplace= True) # elimino la columna duration
+    df['duration_type'].replace('seasons','season', inplace=True) # corrijo los valores "seasons" por "season" para tener un solo valor y facilite la busqueda
+    return df
+
+# Importación de archivos
+df_disney = pd.read_csv (r'Datasets/disney_plus_titles-score.csv', encoding= 'UTF-8')
+df_amazon = pd.read_csv (r'Datasets/amazon_prime_titles-score.csv', encoding= 'UTF-8')
+df_hulu = pd.read_csv (r'Datasets/hulu_titles-score (2).csv', encoding= 'UTF-8')
+df_netflix = pd.read_csv (r'Datasets/netflix_titles-score.csv', encoding= 'UTF-8')
+
+# Aplicación de cambios
+df_amazon = transformation (df_amazon,'a')
+df_disney = transformation (df_disney,'d')
+df_hulu = transformation (df_hulu,'h')
+df_netflix = transformation (df_netflix,'n')
+
+
+#PREGUNTAS
+# 1) Number of times a keyword appears in the title of movies/series, by platform
+def keyword_quantity (data_frame, keyword):
+    contador = 0
+    lista = []
+    for palabra in data_frame.title:
+        palabra = str(palabra).split()
+        for elemento in palabra:
+            lista.append(elemento)
+    contador = lista.count(keyword)
+    return contador
+# 2) Number of films by platform with a score greater than XX in a given year
+def film_by_score(data_frame, anio, puntaje):
+    return len(data_frame[(data_frame['date_added'].dt.year== anio) & (data_frame['score'] >= puntaje)].value_counts())
+# 3) Second highest scoring film for a given platform, based on alphabetical order of titles
+def second_max(data_frame):
+    return data_frame[data_frame['score']==data_frame['score'].max()].sort_values(by=['title']).iloc[1].to_json()
+# 4) Film that lasted the longest according to year, platform and type of duration
+def max_duration(data_frame, tipo):
+    return data_frame[data_frame['duration_type']== tipo].sort_values(by= ['duration_int']).iloc[-1].to_json()
+# 5) Number of series and movies by score
+def movies_series_byscore (data_frame):
+    return data_frame['rating'].value_counts().to_json()
